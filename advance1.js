86cab3b807ee124a25420ba5abc0e161316edd10
Debottam Kar <debottamkar2003@gmail.com>
2025-02-04 23:58:36 +0530

    ascii

--- advance1.js
+++ advance1.js
@@ -120,3 +120,14 @@
 //     await api("rainy");
 //     await api("sunny");
 // })();
+
+function print(){
+    console.log("my name is debottam kar");
+}
+
+function call() {
+    for (let i = 0; i < 5; i++) {
+        setTimeout(print, i * 2000);
+    }
+}
+call();
