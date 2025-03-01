f2a3fbf8fd132455ab10af992b6d93d51ebe7166
Debottam Kar <debottamkar2003@gmail.com>
2025-03-01 19:09:21 +0530

    js and ts concepts

--- ascii.js
+++ ascii.js
@@ -2,9 +2,10 @@
 // Convert ASCII value to character
 let asciiValue = 65; // ASCII value for 'A'
 let character = String.fromCharCode(asciiValue);
-console.log(character); // Output: 'A'
+console.log("ASCII value to charcter: ", character); // Output: 'A'
 // Convert character to ASCII
 let chars = ['a', 'x', 'z', 'b', 'g'];
-for(let i = 0; i < chars.length; i++){
-    console.log(chars[i].charCodeAt(0));
+for (let i = 0; i < chars.length; i++) {
+    console.log("ascii value of: ", chars[i].charCodeAt(0));
 }
+console.log(CharacterData('a'));
