ac51c07fc4386ea3dae8b121aa69f79e8de26742
Debottam Kar <debottamkar2003@gmail.com>
2025-05-18 07:35:49 +0530

    global objects

---
+++ all.js
@@ -0,0 +1,35 @@
+/*
+List of commonly available global objects in Node.js:
+
+- global           // The global namespace object
+- process          // Provides information and control over the current Node.js process
+- Buffer           // Used to handle binary data
+- setTimeout       // Schedules code execution after a set time
+- clearTimeout     // Cancels a timeout
+- setInterval      // Repeats code execution at every set interval
+- clearInterval    // Cancels an interval
+- setImmediate     // Executes code at the end of the current event loop cycle
+- clearImmediate   // Cancels an immediate
+- console          // For printing to stdout and stderr
+- require          // To import modules
+- module           // Reference to the current module
+- exports          // Reference to module exports
+- __dirname        // Directory name of the current module
+- __filename       // File name of the current module
+
+// Example: Print all global property names
+console.log(Object.getOwnPropertyNames(global));
+*/
+
+console.log(global);
+console.log(process);
+console.log(Buffer);
+console.log(setTimeout);
+console.log(clearTimeout);
+console.time("Timer");
+console.log(console);
+console.warn("caution!");
+console.timeEnd("Timer");
+console.log(require);
+console.log(mmodule);
+console.log(exports);
