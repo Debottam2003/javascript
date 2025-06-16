64575cdf761bbe9b9a85a73e067464f9e6ace0c7
Debottam Kar <debottamkar2003@gmail.com>
2025-06-16 23:29:28 +0530

    use form hook

--- func.js
+++ func.js
@@ -1,2 +1,14 @@
-let f = require('./script');
-f();
+// let f = require('./01_js_learn1');
+// console.log(f);
+
+let global_OBJ = {};
+
+function register(name) {
+    global_OBJ["name"] = name;
+    return global_OBJ;
+}
+
+console.log({...register("profile")});
+console.log({...register("id")});
+console.log({...register("email")});
+console.log({...register("password")});
