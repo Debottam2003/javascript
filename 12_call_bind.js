c1740a8ab7630c2f4194af12b2e2a17078adbf6d
Debottam Kar <debottamkar2003@gmail.com>
2025-11-29 15:51:53 +0530

    fixes

--- 12_call_bind.js
+++ 12_call_bind.js
@@ -40,3 +40,38 @@ show.call(user2);
 // console.log(this); // this (->) points to global object
 let func = show.bind(user2);
 func();
+
+// enclosing lexical context
+// In js functions and modules and {let, const} creates lexical scope or enclosing lexical context
+
+const obj1 = {
+    name: "Bob",
+    greet: function () {
+        console.log(this.name);
+    }
+};
+// In regular function this points to the object that called the function
+obj1.greet(); // Output: Bob
+
+const obj2 = {
+    name: "Alice",
+    greet: () => {
+        console.log(this.name);
+    }
+};
+// In arrow function this points to enclosing lexical context
+// In this case, the enclosing lexical context is the global scope where this.name is undefined
+obj2.greet(); // Output: undefined
+
+const obj3 = {
+    name: "Charlie",
+    greet: function () {
+        const innerFunction = () => {
+            console.log(this.name);
+        };
+        innerFunction();
+    }
+};
+// Here, innerFunction is an arrow function, so this inherits from the enclosing lexical context
+// and here enclosing lexical context is greet function where this refers to obj3
+obj3.greet(); // Output: Charlie
