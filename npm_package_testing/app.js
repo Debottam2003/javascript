5f2f0c0e16b493698a3f4e4cd81bc69468e60b98
Debottam Kar <debottamkar2003@gmail.com>
2026-04-26 21:15:19 +0530

    testing v2.0.0 of chatujs

---
+++ npm_package_testing/app.js
@@ -0,0 +1,16 @@
+// import { randomShuffle } from "chatujs";
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// let new_arr = randomShuffle(arr);
+
+// console.log(arr);
+// console.log(new_arr);
+
+// const utils = require("chatujs");
+// console.log(utils);
+// console.log(utils.team);
+// console.log(utils.default);
+
+// const { randomPassword } = require("chatujs");
+// console.log(randomPassword(10));
